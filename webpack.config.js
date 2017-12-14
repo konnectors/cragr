eec8a64991141facee4ec3e1674aff5170a9b6db
Patrick Browne <pt.browne@gmail.com>
2017-12-14 12:39:00 +0100

    chore: use src dir

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   target: 'node',
   output: {
     path: path.join(__dirname, 'build'),
