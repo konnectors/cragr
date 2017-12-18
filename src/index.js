9780f7e8cd86aae1dbfd544ca90bdb98c6776879
doubleface <christophe@cozycloud.cc>
2017-12-18 22:06:48 +0100

    fix: prettier

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-const {BaseKonnector, log, request, errors, updateOrCreate, addData, saveFiles} = require('cozy-konnector-libs')
+const {
+  BaseKonnector,
+  log,
+  request,
+  errors,
+  updateOrCreate,
+  addData,
+  saveFiles
+} = require('cozy-konnector-libs')
 const xlsx = require('xlsx')
 const bluebird = require('bluebird')
 const moment = require('moment')
@@ -25,14 +33,17 @@ module.export = new BaseKonnector(start)
 function start (requiredFields) {
   fields = requiredFields
   return getBankUrl(fields.bankId)
-  .then(login)
-  .then(parseAccounts)
-  .then(saveAccounts)
-  .then(comptes => bluebird.each(comptes, compte => {
-    return fetchOperations(compte)
-    .then(operations => saveOperations(compte, operations))
-  }))
-  .then(getDocuments)
+    .then(login)
+    .then(parseAccounts)
+    .then(saveAccounts)
+    .then(comptes =>
+      bluebird.each(comptes, compte => {
+        return fetchOperations(compte).then(operations =>
+          saveOperations(compte, operations)
+        )
+      })
+    )
+    .then(getDocuments)
 }
 
 function getBankUrl (bankId) {
@@ -60,19 +71,32 @@ function cleanDocumentLabel (label) {
 function getDocuments () {
   log('info', 'Getting accounts statements')
   return fetchStatementPage()
-  .then(parseStatementsPage)
-  .then(accounts => bluebird.each(accounts, fetchAndSaveAccountDocuments))
+    .then(parseStatementsPage)
+    .then(accounts => bluebird.each(accounts, fetchAndSaveAccountDocuments))
 }
 
 function fetchAccountDocuments (account, index) {
-  return rq(account.link)
-  .then($ => {
+  return rq(account.link).then($ => {
     log('info', account.label)
     // now get all the links to the releves of this account
-    const entries = Array.from($('#panneau1 table tbody').eq(index).find('tr[title]')).map(elem => {
+    const entries = Array.from(
+      $('#panneau1 table tbody')
+        .eq(index)
+        .find('tr[title]')
+    ).map(elem => {
       const $cells = $(elem).find('td')
-      const date = $cells.eq(0).text().split('/').reverse().join('')
-      const link = $cells.eq(3).find('a').attr('href').split(';')[1].match(/\('(.*)'\)/)[1]
+      const date = $cells
+        .eq(0)
+        .text()
+        .split('/')
+        .reverse()
+        .join('')
+      const link = $cells
+        .eq(3)
+        .find('a')
+        .attr('href')
+        .split(';')[1]
+        .match(/\('(.*)'\)/)[1]
       return {
         fileurl: `${baseUrl}/stb/${link}&typeaction=telechargement`,
         filename: `releve_${date}_${account.label}.pdf`
@@ -93,8 +117,9 @@ function saveAccountDocuments (entries, index, length) {
 }
 
 function fetchAndSaveAccountDocuments (account, index, length) {
-  return fetchAccountDocuments(account, index)
-  .then(entries => saveAccountDocuments(entries, index, length))
+  return fetchAccountDocuments(account, index).then(entries =>
+    saveAccountDocuments(entries, index, length)
+  )
 }
 
 function parseStatementsPage ($) {
@@ -102,11 +127,22 @@ function parseStatementsPage ($) {
   // here I suppose the fist section is always the releves de comptes section but the name is
   // checked
   log('info', 'Getting the list of accounts with account statements')
-  if ($('#entete1').text().trim() === 'RELEVES DE COMPTES') {
+  if (
+    $('#entete1')
+      .text()
+      .trim() === 'RELEVES DE COMPTES'
+  ) {
     // get the list of accounts with links to display the details
     const accounts = Array.from($('#panneau1 .ca-table tbody')).map(account => {
       const $account = $(account)
-      const label = cleanDocumentLabel($account.find('tr').eq(0).find('a').eq(1).text())
+      const label = cleanDocumentLabel(
+        $account
+          .find('tr')
+          .eq(0)
+          .find('a')
+          .eq(1)
+          .text()
+      )
 
       const link = $account.find('.fleche-ouvrir').attr('href')
       return { label, link: `${baseUrl}/stb/${link}` }
@@ -135,46 +171,53 @@ function fetchOperations (account) {
   return rq({
     url: `${baseUrl}/stb/${account.linkOperations}&typeaction=telechargement`,
     encoding: 'binary'
-  })
-  .then(body => {
+  }).then(body => {
     const workbook = xlsx.read(body, {
       type: 'string',
       raw: true
     })
     const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-    return xlsx.utils.sheet_to_csv(worksheet).split('\n').slice(9).filter(line => {
-      return line.length > 3 // avoid lines with empty cells
-    }).map(line => {
-      const cells = line.split(',')
-      const labels = cells[1].split('\u001b :').map(elem => elem.trim())
+    return xlsx.utils
+      .sheet_to_csv(worksheet)
+      .split('\n')
+      .slice(9)
+      .filter(line => {
+        return line.length > 3 // avoid lines with empty cells
+      })
+      .map(line => {
+        const cells = line.split(',')
+        const labels = cells[1].split('\u001b :').map(elem => elem.trim())
 
-      // select the right cell if it is a debit or a credit
-      let amount = 0
-      if (cells[2].length) {
-        amount = parseFloat(cells[2]) * -1
-      } else if (cells[3].length) {
-        amount = parseFloat(cells[3])
-      } else {
-        log('error', cells, 'Could not find an amount in this operation')
-      }
+        // select the right cell if it is a debit or a credit
+        let amount = 0
+        if (cells[2].length) {
+          amount = parseFloat(cells[2]) * -1
+        } else if (cells[3].length) {
+          amount = parseFloat(cells[3])
+        } else {
+          log('error', cells, 'Could not find an amount in this operation')
+        }
 
-      // some months are abbreviated in French and other in English!!! + encoding problem
-      // TODO use the real csv export (but which is harder to reach) which has better dates
-      const date = cells[0].toLowerCase().replace('dã©c', 'dec').replace('aoã»', 'aug')
+        // some months are abbreviated in French and other in English!!! + encoding problem
+        // TODO use the real csv export (but which is harder to reach) which has better dates
+        const date = cells[0]
+          .toLowerCase()
+          .replace('dã©c', 'dec')
+          .replace('aoã»', 'aug')
 
-      // FIXME a lot of information is hidden in the label of the operation (type of operation,
-      // real date of the operation) but the formating is quite inconsistent
-      return {
-        date: moment(date, 'DD-MMM').toDate(),
-        label: labels.shift(),
-        type: 'none', // TODO parse the labels for that
-        dateImport: new Date(),
-        dateOperation: date, // TODO parse the label for that
-        currency: 'EUR',
-        amount,
-        account: `${account._id}`
-      }
-    })
+        // FIXME a lot of information is hidden in the label of the operation (type of operation,
+        // real date of the operation) but the formating is quite inconsistent
+        return {
+          date: moment(date, 'DD-MMM').toDate(),
+          label: labels.shift(),
+          type: 'none', // TODO parse the labels for that
+          dateImport: new Date(),
+          dateOperation: date, // TODO parse the label for that
+          currency: 'EUR',
+          amount,
+          account: `${account._id}`
+        }
+      })
   })
 }
 
@@ -186,7 +229,8 @@ function parseAccounts ($) {
   log('info', 'Gettings accounts')
   const comptes = Array.from($('.ca-table tbody tr img'))
     .map(compte => $(compte).closest('tr'))
-    .map(compte => Array.from($(compte).find('td'))
+    .map(compte =>
+      Array.from($(compte).find('td'))
         .map(td => {
           const $td = $(td)
           let text = $td.text().trim()
@@ -198,7 +242,10 @@ function parseAccounts ($) {
 
           // if there is an image in the td then get the link to the csv
           if ($td.find('img').length) {
-            text = $td.find('a').attr('href').match(/\('(.*)'\)/)[1]
+            text = $td
+              .find('a')
+              .attr('href')
+              .match(/\('(.*)'\)/)[1]
           }
 
           return text
@@ -225,71 +272,86 @@ function parseAccounts ($) {
 function login (bankUrl) {
   log('info', 'Logging in')
   return rq(`${bankUrl}/particuliers.html`)
-  .then($ => {
-    const script = Array.from($('script'))
-      .map(script => $(script).html().trim())
-      .find(script => {
-        return script.match(/var chemin = "/)
+    .then($ => {
+      const script = Array.from($('script'))
+        .map(script =>
+          $(script)
+            .html()
+            .trim()
+        )
+        .find(script => {
+          return script.match(/var chemin = "/)
+        })
+
+      loginUrl = script.match(/var chemin = "(.*)".*\|/)[1]
+
+      const urlObj = url.parse(loginUrl)
+      baseUrl = `${urlObj.protocol}//${urlObj.hostname}`
+
+      return rq({
+        url: loginUrl,
+        method: 'POST',
+        form: {
+          TOP_ORIGINE: 'V',
+          vitrine: 'O',
+          largeur_ecran: '800',
+          hauteur_ecran: '600',
+          origine: 'vitrine',
+          situationTravail: 'BANQUAIRE',
+          canal: 'WEB',
+          typeAuthentification: 'CLIC_ALLER',
+          urlOrigine: 'http://www.ca-paris.fr',
+          tracking: 'O'
+        }
       })
+    })
+    .then($ => {
+      const touches = Array.from($('#pave-saisie-code td a')).filter(
+        touche =>
+          $(touche)
+            .text()
+            .trim() !== ''
+      )
+      const decodeTable = touches.reduce((memo, touche) => {
+        const $touche = $(touche)
+        memo[$touche.text().trim()] = $touche
+          .closest('td')
+          .attr('onclick')
+          .match(/'(.*)'/)[1]
+        return memo
+      }, {})
 
-    loginUrl = script.match(/var chemin = "(.*)".*\|/)[1]
+      const password = fields.password
+        .split('')
+        .map(nb => decodeTable[nb])
+        .join(',')
 
-    const urlObj = url.parse(loginUrl)
-    baseUrl = `${urlObj.protocol}//${urlObj.hostname}`
-
-    return rq({
-      url: loginUrl,
-      method: 'POST',
-      form: {
-        TOP_ORIGINE: 'V',
-        vitrine: 'O',
-        largeur_ecran: '800',
-        hauteur_ecran: '600',
-        origine: 'vitrine',
-        situationTravail: 'BANQUAIRE',
-        canal: 'WEB',
-        typeAuthentification: 'CLIC_ALLER',
-        urlOrigine: 'http://www.ca-paris.fr',
-        tracking: 'O'
+      return rq({
+        method: 'POST',
+        url: loginUrl,
+        form: {
+          idtcm: '',
+          tracking: 'O',
+          origine: 'vitrine',
+          situationTravail: 'BANCAIRE',
+          canal: 'WEB',
+          typeAuthentification: 'CLIC_RETOUR',
+          idUnique: $('input[name=idUnique]').val(),
+          caisse: $('input[name=caisse]').val(),
+          CCCRYC: password,
+          CCCRYC2: '000000',
+          CCPTE: fields.login
+        }
+      })
+    })
+    .then($ => {
+      const idSessionSag = $('input[name=sessionSAG]').attr('value')
+      statementsUrl = `${baseUrl}/stb/entreeBam?sessionSAG=${idSessionSag}&stbpg=pagePU&act=Edocsynth&stbzn=bnt&actCrt=Edocsynth#null`
+      if ($('.ca-table tbody tr img').length) {
+        log('info', 'LOGIN_OK')
+        return $
+      } else {
+        throw new Error(errors.LOGIN_FAILED)
       }
     })
-  })
-  .then($ => {
-    const touches = Array.from($('#pave-saisie-code td a')).filter(touche => $(touche).text().trim() !== '')
-    const decodeTable = touches.reduce((memo, touche) => {
-      const $touche = $(touche)
-      memo[$touche.text().trim()] = $touche.closest('td').attr('onclick').match(/'(.*)'/)[1]
-      return memo
-    }, {})
-
-    const password = fields.password.split('').map(nb => decodeTable[nb]).join(',')
-
-    return rq({
-      method: 'POST',
-      url: loginUrl,
-      form: {
-        idtcm: '',
-        tracking: 'O',
-        origine: 'vitrine',
-        situationTravail: 'BANCAIRE',
-        canal: 'WEB',
-        typeAuthentification: 'CLIC_RETOUR',
-        idUnique: $('input[name=idUnique]').val(),
-        caisse: $('input[name=caisse]').val(),
-        CCCRYC: password,
-        CCCRYC2: '000000',
-        CCPTE: fields.login
-      }
-    })
-  })
-  .then($ => {
-    const idSessionSag = $('input[name=sessionSAG]').attr('value')
-    statementsUrl = `${baseUrl}/stb/entreeBam?sessionSAG=${idSessionSag}&stbpg=pagePU&act=Edocsynth&stbzn=bnt&actCrt=Edocsynth#null`
-    if ($('.ca-table tbody tr img').length) {
-      log('info', 'LOGIN_OK')
-      return $
-    } else {
-      throw new Error(errors.LOGIN_FAILED)
-    }
-  })
 }
--- src/regions.js
+++ src/regions.js
@@ -5,23 +5,33 @@ const path = require('path')
 
 console.log('Getting the list of regions and the corresponding urls')
 
-request('https://www.credit-agricole.fr')
-.then(body => {
+request('https://www.credit-agricole.fr').then(body => {
   const $ = cheerio.load(body)
   const script = Array.from($('script')).find(script => {
-    return $(script).html().includes('CR_infos_v2')
+    return $(script)
+      .html()
+      .includes('CR_infos_v2')
   })
 
   if (!script) {
     throw new Error('Failed to get the list of available banks')
   }
 
-  const assoc = JSON.parse($(script).html().match(/= ({.*});/)[1])
+  const assoc = JSON.parse(
+    $(script)
+      .html()
+      .match(/= ({.*});/)[1]
+  )
   const result = {}
   for (let key in assoc) {
     result[assoc[key].id_caisse] = assoc[key].url
   }
 
-  fs.writeFileSync(path.join(__dirname, '..', 'regions.json'), JSON.stringify(result, null, 2))
-  console.log(`The list of regions is written in ${path.resolve('../regions.json')}`)
+  fs.writeFileSync(
+    path.join(__dirname, '..', 'regions.json'),
+    JSON.stringify(result, null, 2)
+  )
+  console.log(
+    `The list of regions is written in ${path.resolve('../regions.json')}`
+  )
 })
