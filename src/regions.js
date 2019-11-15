a90c432830502f68b56deb8c587463ae07a6d090
dohseven <tho_jea+framagit@protonmail.com>
2019-11-15 21:37:21 +0100

    Update regions script

--- src/regions.js
+++ src/regions.js
@@ -7,41 +7,59 @@ const path = require('path')
 
 console.log('Getting the list of regions and the corresponding urls')
 
-request('https://www.credit-agricole.fr').then(body => {
-  const $ = cheerio.load(body)
-  const script = Array.from($('script')).find(script => {
-    return $(script)
-      .html()
-      .includes('CR_infos_v2')
-  })
-
-  if (!script) {
-    throw new Error('Failed to get the list of available banks')
-  }
-
-  const assoc = JSON.parse(
-    $(script)
-      .html()
-      .match(/= ({.*});/)[1]
-  )
-  const result = {}
-  const manifestConfig = []
-  for (let key in assoc) {
-    result[assoc[key].id_caisse] = assoc[key].url
-    manifestConfig.push({
-      name: assoc[key].nom,
-      value: assoc[key].id_caisse
+request('https://www.credit-agricole.fr/particulier/acces-cr.html').then(
+  body => {
+    const $ = cheerio.load(body)
+    const script = Array.from($('script')).find(script => {
+      return $(script)
+        .html()
+        .includes('regionalBankId')
     })
+
+    if (!script) {
+      throw new Error('Failed to get the list of available banks')
+    }
+
+    let re = /NPC.listCr\[\d+\] = {regionalBankId: \d+, regionalBankName: "(.*)", regionalBankUrlPrefix: "\/(.*)\/" };/g
+    let m
+    const cr = []
+    do {
+      m = re.exec($(script).html())
+      if (m) {
+        let name = m[1]
+        // Dirty hacks to keep the CR indexes retrocompatibility
+        if (name === 'Paris') name = 'Île-de-France'
+        if (name === 'Nord Est') name = 'Nord-Est'
+        cr.push({
+          name: name,
+          prefix: m[2]
+        })
+      }
+    } while (m)
+
+    cr.sort((a, b) => a.name.localeCompare(b.name))
+
+    let index = 1
+    const regionPrefix = {}
+    const manifestConfig = []
+    for (const value of cr) {
+      regionPrefix[index] = 'https://www.credit-agricole.fr/' + value.prefix
+      manifestConfig.push({
+        name: value.name,
+        value: index.toString()
+      })
+      index++
+    }
+
+    const regionFilePath = path.join(__dirname, '../regions.json')
+    fs.writeFileSync(regionFilePath, JSON.stringify(regionPrefix, null, 2))
+
+    console.log(`The list of regions is written in ${regionFilePath}`)
+
+    manifestConfig.sort((a, b) => a.name.localeCompare(b.name))
+    const manifestFilePath = path.join(__dirname, '../manifestConfig.json')
+    fs.writeFileSync(manifestFilePath, JSON.stringify(manifestConfig, null, 2))
+
+    console.log(`The manifest configuration is written in ${manifestFilePath}`)
   }
-
-  const regionFilePath = path.join(__dirname, '../regions.json')
-  fs.writeFileSync(regionFilePath, JSON.stringify(result, null, 2))
-
-  console.log(`The list of regions is written in ${regionFilePath}`)
-
-  manifestConfig.sort((a, b) => a.name.localeCompare(b.name))
-  const manifestFilePath = path.join(__dirname, '../manifestConfig.json')
-  fs.writeFileSync(manifestFilePath, JSON.stringify(manifestConfig, null, 2))
-
-  console.log(`The manifest configuration is written in ${manifestFilePath}`)
-})
+)
