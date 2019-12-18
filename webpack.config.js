b2a14a614df9a629462ffbe81e32ae0ad55c0f48
doubleface <christophe@cozycloud.cc>
2019-12-18 11:34:39 +0100

    chore: packaging

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,31 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
+const fs = require('fs')
+const SvgoInstance = require('svgo')
+
 const entry = require('./package.json').main
+
+const readManifest = () =>
+  JSON.parse(fs.readFileSync(path.join(__dirname, './manifest.konnector')))
+
+const svgo = new SvgoInstance({
+  plugins: [
+    {
+      inlineStyles: { onlyMatchedOnce: false }
+    }
+  ]
+})
+
+let iconName
+try {
+  iconName = JSON.parse(fs.readFileSync('manifest.konnector', 'utf8')).icon
+  // we run optimize only on SVG
+  if (!iconName.match(/\.svg$/)) iconName = null
+} catch (e) {
+  // console.error(`Unable to read the icon path from manifest: ${e}`)
+}
+const appIconRX = iconName && new RegExp(`[^/]*/${iconName}`)
 
 module.exports = {
   entry,
@@ -16,9 +41,27 @@ module.exports = {
       { from: 'manifest.konnector' },
       { from: 'package.json' },
       { from: 'README.md' },
-      { from: 'assets' },
+      { from: 'assets', transform: optimizeSVGIcon },
       { from: '.travis.yml' },
       { from: 'LICENSE' }
-    ])
-  ]
+    ]),
+    new webpack.DefinePlugin({
+      __WEBPACK_PROVIDED_MANIFEST__: JSON.stringify(readManifest())
+    })
+  ],
+  module: {
+    // to ignore the warnings like :
+    // WARNING in ../libs/node_modules/bindings/bindings.js 76:22-40
+    // Critical dependency: the request of a dependency is an expression
+    // Since we cannot change this dependency. I think it won't hide more important messages
+    exprContextCritical: false
+  }
+}
+
+function optimizeSVGIcon(buffer, path) {
+  if (appIconRX && path.match(appIconRX)) {
+    return svgo.optimize(buffer).then(resp => resp.data)
+  } else {
+    return buffer
+  }
 }
