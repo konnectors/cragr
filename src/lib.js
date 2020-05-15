4fddebc9f10838491f3722b0922b9c12550e81bf
Benoit MERLE <benoitmerle@fserv.fr>
2020-05-16 00:28:00 +0200

    feat: add functions for get documents in new site

--- src/lib.js
+++ src/lib.js
@@ -3,12 +3,12 @@ const {
   requestFactory,
   errors,
   updateOrCreate,
-  //saveFiles,
+  saveFiles,
   cozyClient
 } = require('cozy-konnector-libs')
 const groupBy = require('lodash/groupBy')
 const omit = require('lodash/omit')
-//const bluebird = require('bluebird')
+const bluebird = require('bluebird')
 const moment = require('moment')
 const regions = require('../regions.json')
 const doctypes = require('cozy-doctypes')
@@ -25,15 +25,14 @@ const helpers = require('./helpers')
 const libOldSite = require('./lib_oldsite')
 
 // time given to the connector to save the files
-//const FULL_TIMEOUT = Date.now() + 4 * 60 * 1000
+const FULL_TIMEOUT = Date.now() + 4 * 60 * 1000
 
-/*
 const request = requestFactory({
   // debug: true,
   jar: true,
   json: false,
   cheerio: true
-})*/
+})
 
 const newRequest = requestFactory({
   // debug: true,
@@ -47,8 +46,9 @@ const newRequest = requestFactory({
       'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
   }
 })
+const requestJson = newRequest
 
-//let baseUrl = null
+let baseUrl = null
 let fields = {}
 
 const bankLabel = 'Crédit Agricole'
@@ -61,7 +61,10 @@ const accountDetailsUrl =
   'particulier/operations/synthese/jcr:content.produits-valorisation.json'
 const accountOperationsUrl =
   'particulier/operations/synthese/detail-comptes/jcr:content.n3.operations.json'
-
+const documentsUrl = {
+  getForm: 'particulier/operations/documents/edocuments.html',
+  getToken: 'libs/granite/csrf/token.json'
+}
 let newSite = 0
 
 Document.registerClient(cozyClient)
@@ -76,6 +79,7 @@ let lib
 async function start(requiredFields) {
   fields = requiredFields
   const bankUrl = getBankUrl(fields.bankId)
+  baseUrl = bankUrl
   const accountsPage = await lib.login(bankUrl)
   let accounts = lib.parseAccounts(accountsPage)
   if (newSite) {
@@ -88,16 +92,20 @@ async function start(requiredFields) {
     log('info', `Found ${operations.length} operations`)
     allOperations = allOperations.concat(operations)
   }
-  log('info', allOperations.slice(0, 5), 'operations[0:5]')
+
+  // accounts + operations
   const { accounts: savedAccounts } = await reconciliator.save(
     accounts.map(x => omit(x, ['linkOperations', 'caData'])),
     allOperations
   )
+
+  // balances
   const balances = await fetchBalances(savedAccounts)
   await lib.saveBalances(balances)
-  if (newSite == 0) {
-    await libOldSite.fetchDocuments()
-  }
+
+  // docs
+  if (newSite == 0) await libOldSite.fetchDocuments()
+  else await lib.fetchDocuments()
 }
 
 function getBankUrl(bankId) {
@@ -110,6 +118,286 @@ function getBankUrl(bankId) {
 
   log('info', `Bank url is ${bankUrl}`)
   return bankUrl
+}
+
+/**
+ * call this function for get every documents fr
+ *
+ * @returns {Q.Promise<any> | Promise<void> | PromiseLike<any>}
+ */
+function fetchDocuments() {
+  log('info', 'Getting accounts statements and another documents')
+
+  // get form with identifiantBAM
+  return request(`${baseUrl}/${documentsUrl.getForm}`).then($ => {
+    const form = $('form[name="formulaire"]')
+    const linkPost = helpers.getLinkWithoutBankId(form.attr('action'))
+
+    //log('debug', linkPost, 'linkPost')
+
+    // get token
+    return requestJson(`${baseUrl}/${documentsUrl.getToken}`, {
+      headers: {
+        Referer: `Referer: ${baseUrl}/${documentsUrl.getForm}`
+      }
+    }).then($ => {
+      //log('debug', $.body.token, 'Token')
+
+      const csrf_token = $.body.token
+
+      // get doc page
+      return request(`${baseUrl}/${linkPost}`, {
+        headers: {
+          Referer: `Referer: ${baseUrl}/${documentsUrl.getForm}`
+        },
+        method: 'POST',
+        form: {
+          largeur_ecran: '800',
+          hauteur_ecran: '600',
+          ':cq_csrf_token': csrf_token
+        }
+      })
+        .then(
+          parseStatementsPage
+        ) /*
+        .catch($ => {
+          log(
+            'debug',
+            $.statusCode,
+            'Error, can not get documents | status code'
+          )
+        })*/
+        .then(linkFilesOrDetails =>
+          bluebird.each(linkFilesOrDetails, fetchAndSaveDocuments)
+        )
+    })
+  })
+}
+
+/**
+ * this function is called when document page is finded
+ * (can find every files)
+ *
+ * @param $
+ * @returns {[]|*[]}
+ */
+function parseStatementsPage($) {
+  //log('debug', $, '$')
+  log('info', 'Parsing documents page')
+
+  //if can not find in page
+  if ($('#entete1').length === 0) {
+    log('warning', 'No document find')
+    return []
+  }
+
+  // init
+  let linksFilesOrDetails = []
+
+  for (
+    var indexPanneau = 1, length = 16;
+    indexPanneau <= length;
+    indexPanneau++
+  ) {
+    // for every categories
+    Array.from($('#panneau' + indexPanneau)).forEach(divCategorie => {
+      // for every sub categories
+
+      const $divCategorie = $(divCategorie) // elem to jquery
+
+      const $subCategories_title = $divCategorie.find('.encart-2')
+      const $subCategories_contents = $divCategorie.find('table')
+      const nbSubCategories = $subCategories_title.length
+
+      for (var index = 0; index < nbSubCategories; index++) {
+        const title = $subCategories_title.eq(index).text()
+        const $contents = $subCategories_contents.eq(index)
+
+        // init
+        let labelAccount = ''
+
+        //log('debug', title, 'title')
+        //log('warn', $contents.find('tbody').length, 'tbody length')
+        //log('warn', $contents.find('tbody').eq(0).text(), 'tbody text')
+        //log('warn', $contents.find('tbody').eq(0).find('tr').length, 'tr length')
+
+        Array.from(
+          $contents
+            .find('tbody')
+            .eq(0)
+            .find('tr')
+        ).forEach(tr => {
+          //log('debug', tr, 'tr')
+
+          const $tr = $(tr) // elem to jquery
+
+          //log('warn', $tr.find('.entete-table-compte-repliable').length, '.entete-table-compte-repliable length')
+          //log('warn', $tr.text(), 'tr text')
+
+          // list
+          if ($tr.find('.entete-table-compte-repliable').length > 0) {
+            //log('debug', 'tr list')
+
+            const $div = $tr.find('.entete-table-compte-repliable')
+
+            const link = helpers.getLinkWithoutBankId(
+              $div.find('.fleche-ouvrir').attr('href')
+            )
+
+            labelAccount = helpers.cleanDocumentLabel(
+              $div
+                .find('a')
+                .eq(1)
+                .text()
+            )
+
+            linksFilesOrDetails.push({
+              isFile: false,
+              panelId: '#panneau' + indexPanneau,
+              type: helpers.parseDocumentType(title),
+              account: labelAccount,
+              link: `${baseUrl}/${link}`
+            })
+          }
+          // direct
+          else {
+            //log('debug', 'tr direct');
+
+            const $cells = $tr.find('td')
+            const date = helpers.parseDate($cells.eq(0).text())
+            const link = helpers.parseUrlDownload($cells.eq(3).find('a'))
+            const name = $tr
+              .find('th')
+              .eq(0)
+              .text()
+              .trim()
+
+            linksFilesOrDetails.push({
+              isFile: true,
+              type: helpers.parseDocumentType(title),
+              account: labelAccount,
+              link: `${baseUrl}/${link}`,
+              date: date,
+              name: name
+            })
+          }
+        })
+      }
+    })
+  }
+
+  //log('debug', linksFilesOrDetails, 'list of files or details with their links')
+
+  return linksFilesOrDetails
+}
+
+/**
+ * this function is called when document page is parsed
+ *
+ * @param linkFileOrDetails (contains file link or detail link)
+ * @param index
+ * @param length
+ * @returns {Q.Promise<any> | Promise<void> | PromiseLike<any>}
+ */
+function fetchAndSaveDocuments(linkFileOrDetails, index, length) {
+  //log('debug', linkFileOrDetails, 'linkFileOrDetails')
+  //log('debug', index, 'index')
+  //log('debug', length, 'length')
+
+  /**
+   * parse every return in promise for get real links (when is detail)
+   * and call function for save them
+   */
+  return fetchLinksDocuments(linkFileOrDetails).then(files =>
+    saveDocuments(files, index, length)
+  )
+}
+
+/**
+ * get file name/url for every file or details
+ *
+ * @param linkFileOrDetails
+ * @returns {Q.Promise<any> | Promise<{filename: string, fileurl: string}[]> | PromiseLike<{filename: string, fileurl: string}[]>}
+ */
+function fetchLinksDocuments(linkFileOrDetails) {
+  //log('debug', linkFileOrDetails, 'linkFileOrDetails')
+  // if is file, then return him directly
+  if (linkFileOrDetails.isFile) {
+    const accountDir =
+      linkFileOrDetails.account !== '' ? `/${linkFileOrDetails.account}` : ''
+    return Promise.resolve([
+      {
+        fileurl: `${linkFileOrDetails.link}&typeaction=telechargement`,
+        subPath: `${linkFileOrDetails.type}${accountDir}`,
+        filename: `${linkFileOrDetails.date}_${linkFileOrDetails.name} (${accountDir}).pdf`
+        // filename have ($accountDir) because it's make error if two file have same name
+      }
+    ])
+  }
+
+  // if is details then get files :
+
+  return request(linkFileOrDetails.link).then($ => {
+    log('info', linkFileOrDetails.account, 'get details for')
+    //log('debug', linkFileOrDetails, 'get details for')
+    //log('debug', $, '$')
+
+    // now get all the links to the releves of this account
+    log(
+      'warn',
+      $(linkFileOrDetails.panelId + ' tr[title][id]').length,
+      'nb documents'
+    )
+    return Array.from($(linkFileOrDetails.panelId + ' tr[title][id]')).map(
+      elem => {
+        const $cells = $(elem).find('td')
+        const date = helpers.parseDate($cells.eq(0).text())
+        const link = helpers.parseUrlDownload($cells.eq(3).find('a'))
+        const name = $(elem)
+          .find('th')
+          .eq(0)
+          .text()
+          .trim()
+
+        const accountDir =
+          linkFileOrDetails.account !== ''
+            ? `/${linkFileOrDetails.account}`
+            : ''
+
+        return {
+          fileurl: `${baseUrl}/${link}&typeaction=telechargement`,
+          subPath: `${linkFileOrDetails.type}${accountDir}`,
+          filename: `${date}_${name} (${accountDir}).pdf`
+          // filename have ($accountDir) because it's make error if two file have same name
+        }
+      }
+    )
+  })
+}
+
+/**
+ * save every files
+ *
+ * @param files
+ * @param index
+ * @param length
+ * @returns {*|Promise<[]>}
+ */
+function saveDocuments(files, index, length) {
+  log('debug', files, 'Save files')
+
+  // nothing to save
+  if (files.length === 0) {
+    return null
+  }
+
+  // Give an equal time to fetch documents for each account
+  // next documents will be downloaded for the next run
+  const remainingTime = FULL_TIMEOUT - Date.now()
+  const timeForThisAccount = remainingTime / (length - index)
+  return saveFiles(files, fields, {
+    timeout: Date.now() + timeForThisAccount
+  })
 }
 
 function fillNewAccount(json) {
@@ -392,6 +680,6 @@ module.exports = lib = {
   fetchOperations,
   parseNewOperations,
   syncOperations,
-  //fetchDocuments,
+  fetchDocuments,
   login
 }
